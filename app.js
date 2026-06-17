7bf00a3ea46b7194dfaf664f77dd414f215ee734
iamHaneef <muhamadhaneef.official@gmail.com>
2026-06-17 15:34:37 +0530

    add new folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // immport all important dependencies:
-const express = require("express");
-const cors = require("cors");
-const collections = require("./mongo");
+const express = require('express');
+const cors = require('cors');
+const collections = require('./mongo');
 
 const app = express();
 
@@ -9,143 +9,111 @@ app.use(express.json());
 app.use(cors());
 
 // GET METHOD
-app.get("/" , (req,res) => {
-  res.send("READ ALL DATAS");
+app.get('/', (req, res) => {
+  res.send('READ ALL DATAS');
 });
 
-// POST METHOD -- REGISTER 
-app.post("/register" , async(req,res) => {
+// POST METHOD -- REGISTER
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body; // from frontend
+  const newUser = { email, password };
 
-    const {email , password} = req.body; // from frontend
-    const newUser = { email , password };
-
-    try{
-      const check = await collections.findOne({email : email});
-      if(check)
-      {
-        res.json("Exists");
-      }
-      else
-      {
-        await collections.insertMany([newUser]);
-        res.json("Not Exists");
-      }
+  try {
+    const check = await collections.findOne({ email: email });
+    if (check) {
+      res.json('Exists');
+    } else {
+      await collections.insertMany([newUser]);
+      res.json('Not Exists');
     }
-    catch(error)
-    {
-      res.status(500).json("not exists");
-    }
+  } catch (error) {
+    res.status(500).json('not exists');
+  }
 });
-
 
 // POST METHOD -- LOGIN
-app.post("/login"  , async(req,res) => {
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-  const {email , password} = req.body;
+  try {
+    const check = await collections.findOne({ email: email });
 
-  try{
-    const check = await collections.findOne({ email : email});
-
-    if(check){
-      res.json("EXISTS");
+    if (check) {
+      res.json('EXISTS');
+    } else {
+      res.json('NOT EXISTS');
     }
-    else{
-      res.json("NOT EXISTS");
-    }
+  } catch (err) {
+    res.status(500).json('NOT EXISTS IN SERVER');
   }
-
-  catch(err)
-  {
-    res.status(500).json("NOT EXISTS IN SERVER");
-  }
-
 });
 
 // crud opration - get , post , put , delete:
 
 // create a user
 
-app.post("/users" , async(req,res) => {
-  
-    const {email , password} = req.body ;
-    const newUser = {email , password};
+app.post('/users', async (req, res) => {
+  const { email, password } = req.body;
+  const newUser = { email, password };
 
-      try{
-        const user = await collections.create([newUser]);
-        res.json(user);
-      }
-
-      catch(err)
-      {
-        res.status(400).json("IT DIDN'T CREATE USER");
-      }
-
+  try {
+    const user = await collections.create([newUser]);
+    res.json(user);
+  } catch (err) {
+    res.status(400).json("IT DIDN'T CREATE USER");
+  }
 });
 
 // read the user
 
-app.get("/users" , async(req , res) => {
-
-  try{
-    const user =  await collections.find();
+app.get('/users', async (req, res) => {
+  try {
+    const user = await collections.find();
     res.json(user);
-  }
-
-  catch(err)
-  {
-     res.status(400).json("IT DIDN'T READ USER");
+  } catch (err) {
+    res.status(400).json("IT DIDN'T READ USER");
   }
 });
 
 // read one user by id
 
-app.get("/users/:id" , async(req , res) => {
-
-  try{
-    const user =  await collections.findById(req.params.id);
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await collections.findById(req.params.id);
     res.json(user);
-  }
-
-  catch(err)
-  {
-     res.status(400).json("IT DIDN'T READ that ONE USER");
+  } catch (err) {
+    res.status(400).json("IT DIDN'T READ that ONE USER");
   }
 });
 
 // update the user by id :
 
-app.get("/users/:id" , async(req , res) => {
-
-  try{
-    const updated =  await collections.findByIdAndUpdate(req.params.id , req.body ,
-        {new : "true"}
+app.get('/users/:id', async (req, res) => {
+  try {
+    const updated = await collections.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: 'true' },
     );
     res.json(updated);
-  }
-
-  catch(err)
-  {
-     res.status(400).json("IT DIDN'T UPDATE that ONE USER");
+  } catch (err) {
+    res.status(400).json("IT DIDN'T UPDATE that ONE USER");
   }
 });
 
 // DELETE the user by id :
 
-app.get("/users/:id" , async(req , res) => {
-
-  try{
-    const deleted =  await collections.findByIdAndDelete(req.params.id);
+app.get('/users/:id', async (req, res) => {
+  try {
+    const deleted = await collections.findByIdAndDelete(req.params.id);
     res.json(deleted);
-  }
-
-  catch(err)
-  {
-     res.status(400).json("IT DIDN'T DELETE that ONE USER");
+  } catch (err) {
+    res.status(400).json("IT DIDN'T DELETE that ONE USER");
   }
 });
 
 // server start
 
-app.listen(8000 , ()=>{
-  console.log("SERVER START");
+app.listen(8000, () => {
+  console.log('SERVER START');
 });
